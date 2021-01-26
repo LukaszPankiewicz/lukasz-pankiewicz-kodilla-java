fdfa3df492eca5ff6c5fb2baaffe960a0c0f451d
Lukasz Pankiewicz <pankiewiczlukash@gmail.com>
2021-01-26 12:30:28 +0100

    Preparing for exercise 7.4

---
+++ kodilla-stream/src/test/java/stream/invoice/simple/SimpleInvoiceTestSuite.java
@@ -0,0 +1,23 @@
+package stream.invoice.simple;
+
+import com.kodilla.stream.invoice.simple.SimpleInvoice;
+import com.kodilla.stream.invoice.simple.SimpleItem;
+import com.kodilla.stream.invoice.simple.SimpleProduct;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SimpleInvoiceTestSuite {
+    @Test
+    void testGetValueToPay () {
+        //Given
+        SimpleInvoice invoice = new SimpleInvoice();
+
+        //When
+        invoice.addItem(new SimpleItem(new SimpleProduct("Product 1", 17.28), 2.0));
+        invoice.addItem(new SimpleItem(new SimpleProduct("Product 2", 11.99),3.5));
+        invoice.addItem(new SimpleItem(new SimpleProduct("Product 3", 6.49), 5));
+
+        //Then
+        Assertions.assertEquals(108.975, invoice.getValueToPay(), 0.001);
+    }
+}
