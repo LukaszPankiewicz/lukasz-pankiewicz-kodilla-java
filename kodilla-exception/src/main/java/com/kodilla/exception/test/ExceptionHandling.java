39950c674ab352684ac8e9e9b64050f39a05d428
Lukasz Pankiewicz <pankiewiczlukash@gmail.com>
2021-01-29 15:01:44 +0100

    Incomplete exercise, mission test's in TestSuite

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
@@ -0,0 +1,17 @@
+package com.kodilla.exception.test;
+
+public class ExceptionHandling {
+    public static void main(String[] args) {
+        SecondChallenge secondChallenge = new SecondChallenge();
+        String result = null;
+
+        try {
+            result = secondChallenge.probablyIWillThrowException(10, 15);
+        } catch (Exception e) {
+            System.out.println("Exception: " + e);
+        } finally {
+            System.out.println("End.");
+        }
+    }
+}
+
---
+++ kodilla-exception/src/test/java/com/kodilla/exception/test/SecondChallengeTestSuite.java
@@ -0,0 +1,11 @@
+package com.kodilla.exception.test;
+
+import org.junit.jupiter.api.Test;
+
+public class SecondChallengeTestSuite {
+    @Test
+    void testProbablyIWillThrowException() {
+        //Given
+        SecondChallenge secondChallenge = new SecondChallenge();
+    }
+}
