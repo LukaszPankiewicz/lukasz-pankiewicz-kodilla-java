bafb43d59bb413f1784079939e55dbceb2e3d3d5
Lukasz Pankiewicz <pankiewiczlukash@gmail.com>
2021-01-31 15:10:04 +0100

    Exercise 8.4

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightFinder.java
@@ -0,0 +1,22 @@
+package com.kodilla.exception.test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FlightFinder {
+    void findFlight (Flight flight) throws RouteNotFoundException {
+        Map <String, Boolean> availablePort = new HashMap<>();
+        availablePort.put("Warsaw", true);
+        availablePort.put("Radom", false);
+        availablePort.put("Olsztyn", true);
+        availablePort.put("Katowice", false);
+        availablePort.put("Szczecin", true);
+        availablePort.put("Krakow", false);
+
+        if (availablePort.containsKey(flight.getArrivalAirport())) {
+            System.out.println("Flight to: " + flight.getArrivalAirport() + " is available");
+        } else {
+            throw new RouteNotFoundException("Flight is not available");
+        }
+    }
+}
