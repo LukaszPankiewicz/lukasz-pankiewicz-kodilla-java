ada166a65ab311424516209918683f85a2ad9e3f
Lukasz Pankiewicz <pankiewiczlukash@gmail.com>
2021-03-21 17:30:14 +0100

    Excercise before task #15.4

---
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/factory/Circle.java
@@ -0,0 +1,24 @@
+package com.kodilla.patterns.factory;
+
+public final class Circle implements Shape {
+    private final String name;
+    private final double radius;
+
+    public Circle(String name, double radius) {
+        this.name = name;
+        this.radius = radius;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    public double getArea() {
+        return Math.PI * Math.pow(radius, 2.0);
+    }
+
+    public double getCircumference() {
+        return 2 * Math.PI * radius;
+    }
+}
